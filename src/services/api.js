23fd91dd691a513854b50081c934a158bfff88b5
unknown <carlos.junior@grupoicts.com.br>
2020-05-25 17:28:29 -0400

    env

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://pastelariamao.herokuapp.com/' });
+const api = axios.create({ baseURL: process.env.REACT_APP_PASTELARIA });
 
 export default api;
 
